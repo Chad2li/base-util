f235ec8a094202a9d655baf5bb8ed86577f4d001
chad <li17206@163.com>
2018-02-24 22:27:21 +0800

    common mybatis utils

---
+++ src/main/java/cn/lyjuan/base/mybatis/MyMapper.java
@@ -0,0 +1,22 @@
+package cn.lyjuan.base.mybatis;
+
+
+import org.apache.ibatis.annotations.SelectProvider;
+import tk.mybatis.mapper.common.Mapper;
+import tk.mybatis.mapper.common.MySqlMapper;
+
+/**
+ * 用于以后扩展
+ * @param <T>
+ */
+public interface MyMapper<T> extends Mapper<T>, MySqlMapper<T>
+{
+    /**
+     * 根据Example条件进行查询，并仅返回一条记录，如果有多条记录则抛异常
+     *
+     * @param example
+     * @return
+     */
+    @SelectProvider(type = MyMapperProvider.class, method = "dynamicSQL")
+    T selectOneByExample(Object example);
+}
---
+++ src/main/java/cn/lyjuan/base/mybatis/MyMapperProvider.java
@@ -0,0 +1,24 @@
+package cn.lyjuan.base.mybatis;
+
+import org.apache.ibatis.mapping.MappedStatement;
+import tk.mybatis.mapper.mapperhelper.MapperHelper;
+import tk.mybatis.mapper.provider.ExampleProvider;
+
+public class MyMapperProvider extends ExampleProvider
+{
+    public MyMapperProvider(Class<?> mapperClass, MapperHelper mapperHelper)
+    {
+        super(mapperClass, mapperHelper);
+    }
+
+    /**
+     * 根据Example条件进行查询，并仅返回一条记录，如果有多条记录则抛异常
+     *
+     * @param example
+     * @return
+     */
+    public String selectOneByExample(MappedStatement ms)
+    {
+        return selectByExample(ms);
+    }
+}
